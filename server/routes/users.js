66c5585ba4a6c6735f37dbf536befbc7e389d959
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-30 19:14:48 +0300

    Move routes to auth routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,32 +5,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 const userController = require('../controllers/userController');
 
-passport.serializeUser(function(user, done) {
-  process.nextTick(function() {
-    done(null, { id: user.id, username: user.username });
-  });
-});
-
-passport.deserializeUser(function(user, done) {
-  process.nextTick(function() {
-    return done(null, user);
-  });
-});
-
-passport.use(new LocalStrategy(function verify(username, password, done) {
-  const message = 'Incorrect username or password.';
-  
-  User.findOne({ username })
-    .then(user => {
-      if (!user) return done({ message }, false);
-      
-      if (user.isValidPassword(password)) return done(null, user);
-      done({ message }, false);
-    })
-    .catch(err => done(err))
-}));
-
-router.post('/login', userController.login);
-router.post('/signup', userController.signup);
+router.post('/secureRoute', userController.secureRoute);
+//router.post('/signup', userController.signup);
 
 module.exports = router;

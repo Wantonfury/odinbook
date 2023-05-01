82f2a94fea3046c204ab57c59d3fd987c2af5867
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-01 20:33:43 +0300

    Remove unnecessary packages

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user');
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user');
 const userController = require('../controllers/userController');
 
 router.post('/secureRoute', userController.secureRoute);
-//router.post('/signup', userController.signup);
+router.post('/secureRoute2', userController.secureRoute2);
+router.post('/secureRoute3', userController.secureRoute3);
 
 module.exports = router;

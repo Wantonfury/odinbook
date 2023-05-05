e3cb2af9d60f29402f687a1d357d57f5f985fe4b
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-05 22:25:12 +0300

    Add a login form

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,45 @@
+import '../styles/Form.css';
+import '../styles/Button.css';
+import { useState, useContext } from "react";
+import { login } from "../apis/userAPI";
+import UserContext from '../contexts/UserContext';
+
+const LoginForm = () => {
+  const { setUser } = useContext(UserContext);
+  
+  const [data, setData] = useState({
+    username: '',
+    password: ''
+  });
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    login(data)
+      .then(user => {
+        setUser({
+          ...user,
+          loggedIn: true
+        })
+      })
+      .catch(err => console.log(err));
+  }
+  
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+      <button className="btn" type="submit">Log in</button>
+      <button className="btn" type="button">Sign up</button>
+    </form>
+  );
+}
+
+export default LoginForm;

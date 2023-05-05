21294f6e528ddbc57220f8305a7ece991c9d38f9
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-05 22:24:20 +0300

    Add LoginPage, UserPage and user state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import './styles/App.css';
+import './styles/Container.css';
+import LoginPage from './components/LoginPage';
+import UserContext from './contexts/UserContext';
+import { useState, useEffect } from "react";
+import UserPage from './components/UserPage';
+
+const defaultUser = {
+  username: '',
+  loggedIn: false
+}
 
 function App() {
+  const [user, setUser] = useState(defaultUser);
+  
   return (
-    <div className="App">
-      
-    
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="App">
+        {user.loggedIn ? <UserPage /> : <LoginPage />}
+      </div>
+    </UserContext.Provider>
   );
 }
 

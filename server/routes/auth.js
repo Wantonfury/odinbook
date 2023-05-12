48ddd4844d85853615f49a87758ffae8d9c00f7a
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-13 00:23:30 +0300

    Add isLoggedIn to auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,5 +6,6 @@ router.post('/signup', authController.signup);
 router.post('/logout', authController.logout);
 
 router.post('/login', authController.login);
+router.get('/logged_in', authController.isLoggedIn);
 
 module.exports = router;

acd0769580c1975760dd9b31c242c4e28d4dea1a
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-13 00:16:48 +0300

    Add loading and auto-relog on refresh

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import './styles/Container.css';
 import LoginPage from './components/LoginPage';
 import UserContext from './contexts/UserContext';
 import { useState, useEffect } from "react";
-import UserPage from './components/UserPage';
+import HomePage from './components/HomePage';
+import LoadingIcon from './components/LoadingIcon';
+import { checkLogin } from './apis/userAPI';
 
 const defaultUser = {
   username: '',
@@ -12,11 +14,27 @@ const defaultUser = {
 
 function App() {
   const [user, setUser] = useState(defaultUser);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    checkLogin()
+      .then(res => {
+        console.log(res.data);
+        setUser({
+          ...res.data,
+          loggedIn: true
+        })
+      })
+      .catch(() => {
+        setUser({ loggedIn: false });
+      })
+      .finally(() => setLoading(false));
+  }, []);
   
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <div className="App">
-        {user.loggedIn ? <UserPage /> : <LoginPage />}
+        {loading ? <LoadingIcon /> : user.loggedIn ? <HomePage /> : <LoginPage />}
       </div>
     </UserContext.Provider>
   );

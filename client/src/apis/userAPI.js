7c6b4cd703f6381b0bd6c0d432bf50cf132fdadd
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-13 00:17:01 +0300

    Add api calls for users

--- client/src/apis/userAPI.js
+++ client/src/apis/userAPI.js
@@ -1,20 +1,36 @@
 import axios from "axios";
 const SERVER = process.env.REACT_APP_SERVER;
 
-const login = (data) => {
-  return axios.post(`${SERVER}/auth/login`, data, { withCredentials: true });
+axios.defaults.withCredentials = true;
+
+export const login = (data) => {
+  return axios.post(`${SERVER}/auth/login`, data);
 }
 
-const logout = () => {
-  return axios.post(`${SERVER}/auth/logout`, { withCredentials: true });
+export const logout = () => {
+  return axios.post(`${SERVER}/auth/logout`);
 }
 
-const signup = () => {
+export const signup = () => {
   
 }
 
-export {
-  login,
-  logout,
-  signup
-};
+export const checkLogin = () => {
+  return axios.get(`${SERVER}/auth/logged_in`);
+}
+
+export const getNonFriends = (start, end) => {
+  return axios.get(`${SERVER}/users/get_non_friends`, { start, end });
+}
+
+export const getPendingFriends = () => {
+  return axios.get(`${SERVER}/users/get_pending_friends`);
+}
+
+export const getFriends = () => {
+  return axios.get(`${SERVER}/users/get_friends`);
+}
+
+export const addFriend = (id) => {
+  return axios.post(`${SERVER}/users/add_friend`, { id });
+}

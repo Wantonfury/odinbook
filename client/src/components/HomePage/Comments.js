482b9976e86fba8b5d41dcb31b7c03581d000eea
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-13 00:19:03 +0300

    Add a comments section

---
+++ client/src/components/HomePage/Comments.js
@@ -0,0 +1,14 @@
+import '../../styles/Comments.css';
+import CommentForm from './CommentForm';
+
+
+
+const Comments = () => {
+  return (
+    <div id="comments">
+      <CommentForm />
+    </div>
+  );
+}
+
+export default Comments;

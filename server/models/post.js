86303d71df891bdee54767701ccb369198bd0af9
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-23 12:22:02 +0300

    Replace comments with posts

---
+++ server/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  message: { type: String, minLength: 5, maxLength: 255, required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  date: { type: Date, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  comments: [{
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    comment: { type: String, minLength: 5, maxLength: 100, required: true }
+  }]
+});
+
+module.exports = mongoose.model('Post', PostSchema);

85ff8c2e573a5bf08a8e862e35ca280b43da700b
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-23 12:12:35 +0300

    Add UserPage

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import { useState, useEffect } from "react";
 import HomePage from './components/HomePage';
 import LoadingIcon from './components/LoadingIcon';
 import { checkLogin } from './apis/userAPI';
+import { ModalProvider } from './contexts/ModalContext';
+import Header from './components/Header';
+import UserPage from './components/UserPage';
 
 const defaultUser = {
   username: '',
@@ -15,11 +18,11 @@ const defaultUser = {
 function App() {
   const [user, setUser] = useState(defaultUser);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(<HomePage />);
   
   useEffect(() => {
     checkLogin()
       .then(res => {
-        console.log(res.data);
         setUser({
           ...res.data,
           loggedIn: true
@@ -31,11 +34,31 @@ function App() {
       .finally(() => setLoading(false));
   }, []);
   
+  // const handlePage = () => {
+  //   if (user.userPageId) {
+  //     return <UserPage />;
+  //   }
+    
+  //   return <HomePage />;
+  // }
+  
+  useEffect(() => {
+    if (user.userPageId) setCurrentPage(<UserPage />);
+    else setCurrentPage(<HomePage />);
+  }, [user.userPageId]);
+  
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <div className="App">
-        {loading ? <LoadingIcon /> : user.loggedIn ? <HomePage /> : <LoginPage />}
-      </div>
+      <ModalProvider>
+        <div className="App">
+          {loading ? null : <Header />}
+          
+          {
+            loading ? <LoadingIcon /> : 
+              user.loggedIn ? currentPage : <LoginPage />
+          }
+        </div>
+      </ModalProvider>
     </UserContext.Provider>
   );
 }

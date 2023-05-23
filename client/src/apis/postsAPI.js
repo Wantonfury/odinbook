53142d9f0b81a8fe7ad9d3ee214f044c5f0f98bb
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-23 12:12:52 +0300

    Add post database calls

---
+++ client/src/apis/postsAPI.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+const SERVER = process.env.REACT_APP_SERVER;
+
+axios.defaults.withCredentials = true;
+
+export const post = (data) => {
+  return axios.post(`${SERVER}/posts/post`, data);
+}
+
+export const getPosts = () => {
+  return axios.get(`${SERVER}/posts/get_posts_all`);
+}
+
+export const getPostsUser = (id) => {
+  return axios.get(`${SERVER}/posts/get_posts_user?id=${id}`);
+}
+
+export const getComments = (id) => {
+  return axios.get(`${SERVER}/posts/get_comments?id=${id}`);
+}
+
+export const addComment = (id, comment) => {
+  return axios.post(`${SERVER}/posts/add_comment`, { id, comment });
+}
+
+export const addLike = (data) => {
+  return axios.post(`${SERVER}/posts/like`, data);
+}

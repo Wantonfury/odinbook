0854280c446b356208ffd86ad194dfcb3871466a
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-23 12:17:36 +0300

    Add a dummy unfriend button

---
+++ client/src/components/UserPage/BtnUnfriend.js
@@ -0,0 +1,9 @@
+import '../../styles/Button.css';
+
+const BtnUnfriend = (props) => {
+  return (
+    <button type='button' className='btn'>Unfriend</button>
+  );
+}
+
+export default BtnUnfriend;

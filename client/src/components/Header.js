f156b9961dd3d01cbb0d6c5996e90e9fb679b341
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-23 12:13:55 +0300

    Add a header with a home button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,29 @@
+import '../styles/Header.css';
+import UserProfilePicture from './UserProfilePicture';
+import HomeIcon from '../assets/images/home.svg';
+import { useContext } from "react";
+import UserContext from '../contexts/UserContext';
+
 const Header = () => {
-  return;
+  const { user, setUser } = useContext(UserContext);
+  
+  const handleHome = () => {
+    setUser({
+      ...user,
+      userPageId: null
+    });
+  }
+  
+  return (
+    <div id="header">
+      <div className='header-left'>
+        <img className='header-home' src={HomeIcon} alt="Home Page" onClick={handleHome} />
+        <input type='text' />
+      </div>
+      
+      <UserProfilePicture id={user.id} />
+    </div>
+  );
 }
 
 export default Header;

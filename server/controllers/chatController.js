0fb0ae6c424ed3a39c939158469788adedde8b00
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 15:12:57 +0300

    Add a controller for chats

---
+++ server/controllers/chatController.js
@@ -0,0 +1,66 @@
+const Chat = require('../models/chat');
+const { body, validationResult } = require('express-validator');
+const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
+
+exports.getMessages = (req, res, next) => {
+  Chat.findOne({ $and: [
+    { users: req.user._id },
+    { users: req.query.id }
+  ]}).populate('messages.user', 'messages.user first_name last_name')
+    .then(chat => {
+      res.status(200).json(!chat ? [] : chat.messages)
+    })
+    .catch(err => next(err));
+}
+
+exports.addMessage = [
+  body('message', 'Message must be between 1 and 100 characters.')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array({ onlyFirstError: true }).map(err => err.msg));
+    }
+    
+    try {
+      let chat = await Chat.findOne({ $and: [
+        { users: req.user._id },
+        { users: req.body.id }
+      ] });
+      
+      const message = {
+        message: req.body.message,
+        user: req.user._id,
+        date: Date.now(),
+        read: []
+      };
+      
+      if (chat) {
+        console.log(chat);
+        chat.messages.push(message);
+      } else {
+        if (!chat) chat = new Chat({
+          users: [req.user._id, req.body.id],
+          messages: [message]
+        });
+      }
+      
+      
+      
+      chat.save()
+        .then(() => res.status(200).send())
+        .catch(err => next(err));
+    } catch(err) {
+      next(err);
+    }
+  }
+]
+
+exports.readMessages = (req, res, next) => {
+  
+}

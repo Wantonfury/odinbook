1e073492d0a7418c033a561ce32f3c009687228c
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 23:09:37 +0300

    Check for unread messages and add notification

--- client/src/components/UserNameplate.js
+++ client/src/components/UserNameplate.js
@@ -1,10 +1,31 @@
 import DefaultProfileImage from '../assets/images/account-default-image.svg';
 import '../styles/Contacts.css';
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from '../contexts/UserContext';
+import { getUnreadMessagesCount } from '../apis/chatAPI';
 
 const UserNameplate = (props) => {
   const { user, setUser } = useContext(UserContext);
+  const [unreadMessages, setUnreadMessages] = useState();
+  const [updateUnreadMessages, setUpdateUnreadMessages] = useState(false);
+  
+  useEffect(() => {
+    getUnreadMessagesCount(props.user.id)
+      .then(res => {
+        setUnreadMessages(res.data);
+        setUpdateUnreadMessages(false);
+      });
+  }, [props.user, updateUnreadMessages]);
+  
+  useEffect(() => {
+    if (user.updateRead === props.user.id) {
+      setUpdateUnreadMessages(true);
+      setUser({
+        ...user,
+        updateRead: false
+      })
+    }
+  }, [user.updateRead, props.user, setUser]);
   
   const handleClick = () => {
     setUser({
@@ -14,9 +35,16 @@ const UserNameplate = (props) => {
   }
   
   return (
-    <div className="contact" onClick={handleClick}>
+    <div className={`contact ${unreadMessages > 0 ? 'contact-notification' : ''}`} onClick={handleClick}>
       <img src={props.user.pfp ? props.user.pfp : DefaultProfileImage} alt='Profile' />
       <span>{ props.user.first_name + ' ' + props.user.last_name }</span>
+      <p style={{
+        textAlign: 'right',
+        margin: '0',
+        marginLeft: 'auto',
+        padding: '0',
+        paddingRight: '8px'
+      }}>{unreadMessages === 0 ? '' : unreadMessages}</p>
     </div>
   );
 }

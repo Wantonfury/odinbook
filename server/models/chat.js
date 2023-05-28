e8b5c214107572e6fa5a073e49d0c29314a5f69d
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 15:13:44 +0300

    Add a model for chat

---
+++ server/models/chat.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ChatSchema = new Schema({
+  users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+  messages: [{
+    message: { type: String, minLength: 1, maxLength: 100, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+    date: { type: Date, required: true },
+    read: [{ type: Boolean, required: true }]
+  }]
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);

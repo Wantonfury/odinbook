db6a95bd96cd2b348dae85a48573af4b274ea393
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 15:12:49 +0300

    Add a router for chats

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ const cookieOpts = require('./utils/settings').cookieOpts;
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const authRouter = require('./routes/auth');
+const chatsRouter = require('./routes/chats');
 
 const mongoDB = process.env.MONGODB_URI;
 
@@ -68,6 +69,7 @@ passport.deserializeUser((user, done) => {
 app.use('/auth', authRouter);
 app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/posts', ensureAuthenticated, postsRouter);
+app.use('/chats', ensureAuthenticated, chatsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

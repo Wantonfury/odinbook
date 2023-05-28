1fb913dbf99bde91e20a9b8eb2ad7f68f56efed3
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 23:12:03 +0300

    Add path for unread messages count

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const chatController = require('../controllers/chatController');
 
 router.get('/get_messages', chatController.getMessages);
+router.get('/get_unread_messages_count', chatController.getUnreadMessagesCount);
+
 router.post('/add_message', chatController.addMessage);
 router.post('/read_messages', chatController.readMessages);
 

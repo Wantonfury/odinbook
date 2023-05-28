68e30a7f98f6401bddfbab307c8c02d0405b59b4
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 23:09:06 +0300

    Add api calls for read and unread messages

--- client/src/apis/chatAPI.js
+++ client/src/apis/chatAPI.js
@@ -7,6 +7,14 @@ export const getMessages = (id) => {
   return axios.get(`${SERVER}/chats/get_messages?id=${id}`);
 }
 
+export const getUnreadMessagesCount = (id) => {
+  return axios.get(`${SERVER}/chats/get_unread_messages_count?id=${id}`);
+}
+
 export const addMessage = (message, id) => {
   return axios.post(`${SERVER}/chats/add_message`, { message, id });
+}
+
+export const readMessages = (id, messages) => {
+  return axios.post(`${SERVER}/chats/read_messages`, { id, messages });
 }

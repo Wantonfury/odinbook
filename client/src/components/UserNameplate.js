8904bb2284de75462826721aaa0f7af15ad4034d
Wantonfury <orzan.alexandru94@gmail.com>
2023-05-28 15:11:11 +0300

    Add onClick and its handle to open chat

--- client/src/components/UserNameplate.js
+++ client/src/components/UserNameplate.js
@@ -1,9 +1,20 @@
 import DefaultProfileImage from '../assets/images/account-default-image.svg';
 import '../styles/Contacts.css';
+import { useContext } from "react";
+import UserContext from '../contexts/UserContext';
 
 const UserNameplate = (props) => {
+  const { user, setUser } = useContext(UserContext);
+  
+  const handleClick = () => {
+    setUser({
+      ...user,
+      chatbox: props.user.id
+    })
+  }
+  
   return (
-    <div className="contact">
+    <div className="contact" onClick={handleClick}>
       <img src={props.user.pfp ? props.user.pfp : DefaultProfileImage} alt='Profile' />
       <span>{ props.user.first_name + ' ' + props.user.last_name }</span>
     </div>

bde305be958e88d7d5ce447da6553e353add968b
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-05 15:05:21 +0300

    Add socket logic and styling

--- client/src/components/ChatBox/ChatBox.js
+++ client/src/components/ChatBox/ChatBox.js
@@ -1,19 +1,51 @@
 import '../../styles/ChatBox.css';
 import '../../styles/Form.css';
 import '../../styles/Button.css';
-import { useContext } from "react";
+import '../../styles/SVG.css';
+import { useContext, useState, useEffect } from "react";
 import UserContext from '../../contexts/UserContext';
 import ChatLog from './ChatLog';
 import ChatInput from './ChatInput';
+import { getChatId } from '../../apis/chatAPI';
+import SocketContext from '../../contexts/SocketContext';
+import IconClose from '../../assets/images/close-circle.svg';
+import ChatContext from '../../contexts/ChatContext';
 
 const ChatBox = () => {
   const { user, setUser } = useContext(UserContext);
+  const { chatBoxId, setChatBoxId } = useContext(ChatContext);
+  const { socket } = useContext(SocketContext);
+  const [chatId, setChatId] = useState(null);
+  
+  useEffect(() => {
+    if (!chatBoxId) return;
+    
+    getChatId(chatBoxId)
+      .then(res => {
+        if (chatId) socket.emit('leave_chat', chatId);
+        setChatId(res.data);
+        socket.emit('join_chat', { chat: res.data });
+      });
+  }, [chatBoxId, socket, chatId]);
+  
+  const handleClose = (e) => {
+    // setUser({
+    //   ...user,
+    //   chatbox: null
+    // })
+    setChatBoxId(null);
+  }
   
   return (
     <div className="chatbox card">
+      <input type='image' className='svg' src={IconClose} alt='Close Chat' onClick={handleClose} style={{
+        width: '30px',
+        height: 'auto',
+        marginLeft: 'auto'
+      }} />
       <ChatLog user={user} setUser={setUser} />
       
-      <ChatInput user={user} />
+      <ChatInput chatId={chatId} />
     </div>
   );
 }

9def2315270bf03009232bece810dda2d4c60c20
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-05 15:09:38 +0300

    Add get_chat_id route

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -3,6 +3,7 @@ const chatController = require('../controllers/chatController');
 
 router.get('/get_messages', chatController.getMessages);
 router.get('/get_unread_messages_count', chatController.getUnreadMessagesCount);
+router.get('/get_chat_id', chatController.getChatId);
 
 router.post('/add_message', chatController.addMessage);
 router.post('/read_messages', chatController.readMessages);

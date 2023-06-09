cb32fc667fe209c6a92f35d0bd19fedafd948c78
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-09 19:59:42 +0300

    Add post loading when the user scrolls

--- client/src/components/UserPage/UserPosts.js
+++ client/src/components/UserPage/UserPosts.js
@@ -8,6 +8,8 @@ import UserContext from '../../contexts/UserContext';
 const UserPosts = (props) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [postsLoaded, setPostsLoaded] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
   const { userPageId } = useContext(UserContext);
   
   useEffect(() => {
@@ -21,14 +23,44 @@ const UserPosts = (props) => {
       .finally(() => setLoading(false));
   }, [loading, userPageId]);
   
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.scrollingElement.scrollHeight) {
+        if (postsLoaded) return;
+        
+        setLoadingMore(true);
+        
+        getPostsUser(userPageId, posts && posts.length > 0 ? posts[posts.length - 1].date : null)
+          .then(res => {
+            if (res.data && res.data.length === 0) setPostsLoaded(true);
+            setPosts(res.data ? posts.concat(res.data) : posts);
+          })
+          .finally(() => setLoadingMore(false));
+      }
+    }
+    
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [posts, userPageId, postsLoaded]);
+  
   return (
     <div className="posts posts-narrow">
       {
         loading ? <LoadingIcon /> :
           (!posts || posts.length === 0) ? null :
-            posts.map((post, index) => {
-              return <Post key={index} post={post} />;
-            })
+          <>
+            {
+              posts.map((post, index) => {
+                return <Post key={index} post={post} />;
+              })
+            }
+            {
+              loadingMore ? <LoadingIcon /> : null
+            }
+          </>
       }
     </div>
   );

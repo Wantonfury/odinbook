9515d2421d1ea2f1bafb2303705f08d2343d2d9d
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-09 19:56:22 +0300

    Add photos to posts and display likes and comments

--- client/src/components/HomePage/Post.js
+++ client/src/components/HomePage/Post.js
@@ -1,50 +1,62 @@
-import { addLike } from "../../apis/postsAPI";
+import { addLike, getCommentsCount } from "../../apis/postsAPI";
 import Comments from "./Comments";
 import { useState } from "react";
 import UserProfilePicture from "../UserProfilePicture";
 import UserName from "../UserName";
+import IconThumbUp from '../../assets/images/thumb-up.svg';
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
 
-const Post = (props) => {
+const Post = ({ post }) => {
   const [showComments, setShowComments] = useState(false);
   
   const handleLike = (e) => {
     e.target.classList.contains('liked') ? e.target.classList.remove('liked') : e.target.classList.add('liked');
     
-    addLike(props.post.id)
+    addLike(post.id)
       .then(res => {
         res.data.liked ? e.target.classList.add('liked') : e.target.classList.remove('liked');
       });
   }
 
   const handleComments = () => {
-    //e.target.parentNode.parentNode.querySelector('.post-reply').classList.toggle('post-disabled');
     setShowComments(!showComments);
   }
   
   return (
-    <div className="card post">
-      <div>
+    <div className="card">
+      <div className='post'>
         <div className="post-user">
-          <UserProfilePicture pfp={props.post.user.pfp} id={props.post.user.id} />
+          <UserProfilePicture pfp={post.user.pfp} id={post.user.id} />
           
           <div className="post-user-info">
-            <UserName full_name={`${props.post.user.first_name} ${props.post.user.last_name}`} id={props.post.user.id} />
-            <span>{props.post.date}</span>
+            <UserName full_name={`${post.user.first_name} ${post.user.last_name}`} id={post.user.id} />
+            <span>{dayjs(post.date).fromNow()}</span>
           </div>
         </div>
         
-        <p>{props.post.message}</p>
+        <span className='post-message'>{post.message}</span>
+        { post.photo && post.photo.length > 0 ? <img className='post-photo' src={`${process.env.REACT_APP_SERVER}/${post.photo}`} alt='Post' /> : null}
+        
+        <div className="post-details">
+          <div className='post-likes'>
+            <img src={IconThumbUp} alt='Like' />
+            <span>{post.likes}</span>
+          </div>
+          <span className='post-comments' onClick={handleComments}>{post.commentsCount} comments</span>
+        </div>
       </div>
       
       <hr />
       
       <div className="post-options">
-        <button className={"btn btn-transparent" + (props.post.liked ? ' liked' : '')} type="button" onClick={handleLike}>Like</button>
+        <button className={"btn btn-transparent" + (post.liked ? ' liked' : '')} type="button" onClick={handleLike}>Like</button>
         <button className="btn btn-transparent" type="button" onClick={handleComments}>Comment</button>
       </div>
       
       {
-        showComments ? <Comments id={props.post.id} /> : null
+        showComments ? <Comments id={post.id} /> : null
       }
     </div>
   );

72f418101ae2ee62d6da3105417733433bd7dc3e
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-11 03:50:20 +0300

    Remove deprecated api calls

--- client/src/apis/chatAPI.js
+++ client/src/apis/chatAPI.js
@@ -7,20 +7,12 @@ export const getMessages = (id, fromDate = null) => {
   return axios.get(`${SERVER}/chats/get_messages`, { params: { id, fromDate }});
 }
 
-export const getMessagesUnread = (id) => {
-  return axios.get(`${SERVER}/chats/get_messages_unread`, { params: { id }});
-}
-
 export const getUnreadMessagesCount = (id) => {
   return axios.get(`${SERVER}/chats/get_unread_messages_count?id=${id}`);
 }
 
 export const addMessage = (message, id) => {
   return axios.post(`${SERVER}/chats/add_message`, { message, id });
-}
-
-export const readMessages = (id, messages) => {
-  return axios.post(`${SERVER}/chats/read_messages`, { id, messages });
 }
 
 export const getChatId = (id) => {

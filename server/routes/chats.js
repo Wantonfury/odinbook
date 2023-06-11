ba8d58fb414fbd7639f231fbca55731aa27e2a21
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-11 03:51:56 +0300

    Remove deprecated routes

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const chatController = require('../controllers/chatController');
 
 router.get('/get_messages', chatController.getMessages);
-router.get('/get_messages_unread', chatController.getMessagesUnread);
 router.get('/get_unread_messages_count', chatController.getUnreadMessagesCount);
 router.get('/get_chat_id', chatController.getChatId);
 
 router.post('/add_message', chatController.addMessage);
-router.post('/read_messages', chatController.readMessages);
 
 module.exports = router;

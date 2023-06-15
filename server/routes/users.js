f3a94a33138c2e0888887eaf572ae892662ef380
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-15 19:14:07 +0300

    Add remove_friend route

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,11 +5,12 @@ const userController = require('../controllers/userController');
 router.get('/get_non_friends', userController.getNonFriends);
 router.get('/get_pending_friends', userController.getPendingFriends);
 router.get('/search_users', userController.searchUsers);
-
 router.get('/get_friends', userController.getFriends);
 router.get('/get_friends_user', userController.getFriendsUser);
 router.get('/get_user', userController.getUser);
+
 router.post('/add_friend', userController.addFriend);
+router.post('/remove_friend', userController.removeFriend);
 router.post('/change_name', userController.changeName);
 
 

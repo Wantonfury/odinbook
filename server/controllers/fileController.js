4b12306f51695c914fe206e279ddd29d29e66b85
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-15 18:07:03 +0300

    Add a check to see if user provided a file

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -3,6 +3,7 @@ const User = require('../models/user');
 exports.uploadProfileFile = (req, res, next) => {
   User.findOne({ _id: req.user._id })
     .then(user => {
+      if (!req.file) return res.status(400).json({ errors: ['Please provide a new profile picture.']})
       user.pfp = req.file.path;
       
       user.save()

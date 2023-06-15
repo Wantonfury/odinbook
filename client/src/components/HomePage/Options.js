45212024a62c61351da10a3556e6324eadae9ca4
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-15 19:13:17 +0300

    Add Friends button

--- client/src/components/HomePage/Options.js
+++ client/src/components/HomePage/Options.js
@@ -5,6 +5,7 @@ const Options = ({ setOption, options }) => {
   return (
     <div id="options">
       <button className='btn' type='button' onClick={() => setOption(options.recent)}>Recent</button>
+      <button className='btn' type='button' onClick={() => setOption(options.friends)}>Friends</button>
       <button className='btn' type='button' onClick={() => setOption(options.findFriends)}>Find Friends</button>
     </div>
   );

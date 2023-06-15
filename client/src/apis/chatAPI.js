924cea1e189e259c65bc74a7e2a28d6c3c3de9f9
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-15 13:14:10 +0300

    Change chats to message, and use id for most calls

--- client/src/apis/chatAPI.js
+++ client/src/apis/chatAPI.js
@@ -4,17 +4,17 @@ const SERVER = process.env.REACT_APP_SERVER;
 axios.defaults.withCredentials = true;
 
 export const getMessages = (id, fromDate = null) => {
-  return axios.get(`${SERVER}/chats/get_messages`, { params: { id, fromDate }});
+  return axios.get(`${SERVER}/messages/get_messages`, { params: { id, fromDate }});
 }
 
 export const getUnreadMessagesCount = (id) => {
-  return axios.get(`${SERVER}/chats/get_unread_messages_count?id=${id}`);
+  return axios.get(`${SERVER}/messages/get_unread_messages_count`, { params: { id } });
 }
 
-export const addMessage = (message, id) => {
-  return axios.post(`${SERVER}/chats/add_message`, { message, id });
+export const addMessage = (id, message) => {
+  return axios.post(`${SERVER}/messages/add_message`, { id, message });
 }
 
-export const getChatId = (id) => {
-  return axios.get(`${SERVER}/chats/get_chat_id?id=${id}`);
+export const getChatId = (users) => {
+  return axios.get(`${SERVER}/messages/get_chat_id`, { params: { users } });
 }

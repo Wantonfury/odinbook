c051c3ac8bad8937aaacde9143d20c64af2c2e91
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-18 19:37:11 +0300

    Add file filter for img type and file size

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -16,7 +16,20 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const fileSize = parseInt(req.headers["content-length"]);
+    
+    if (file.mimetype !== 'image/jpeg' || file.mimetype !== 'image/jpg' || file.mimetype !== 'image/png' || file.mimetype !== 'image/webp' || file.mimetype !== 'image/svg') {
+      return cb('File type must be .jpg, .jpeg, .png, .webp or .svg.', false);
+    } else if (fileSize > 16777216) {
+      return cb('File size must be below 16mb.', false);
+    }
+    
+    cb(null, true);
+  }
+});
 
 exports.uploadFolder = dest.slice(1);
 exports.upload = upload;

1ba40ec9809d3e091b4c70ab517780709a8553cc
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-18 21:20:11 +0300

    Add signup validation and change err to errors

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -15,12 +15,40 @@ exports.validation = [
     if (!errors.isEmpty()) {
       return res.status(400).json({
         msg: 'Validation Error',
-        err: errors.array({ onlyFirstError: true }).map(err => err.msg)
+        errors: errors.array({ onlyFirstError: true }).map(err => err.msg)
       });
     }
     
     next();
   }
 ];
+
+exports.validationSignup = [
+  body('first_name', 'First name must be between 1 and 100 characters.')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape(),
+  body('last_name', 'Last name must be between 1 and 100 characters.')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape(),
+  body('age', 'Age must be at least 13.')
+    .trim()
+    .isNumeric()
+    .isInt({ min: 13 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        msg: 'Validation Error',
+        errors: errors.array({ onlyFirstError: true }).map(err => err.msg)
+      });
+    }
+    
+    next();
+  }
+]
 
 exports.ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.status(401).send();

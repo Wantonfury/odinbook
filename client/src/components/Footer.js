1f1d66eff1dba7f620a36538021d69d3f218791b
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-21 20:53:58 +0300

    Add ThemeContext for github icon

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,15 @@
-import IconGithub from '../assets/images/github-mark.png';
+import { useContext } from 'react';
 import '../styles/Footer.css';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const Footer = () => {
+  const { icons } = useContext(ThemeContext);
+  
   return (
     <div id='footer'>
       <span>Created by Wantonfury</span>
       <a href='https://github.com/Wantonfury/odinbook' target='_blank' rel='noreferrer noopener'>
-        <img src={IconGithub} alt='Link to GitHub' />
+        <img src={icons.github} alt='Link to GitHub' />
       </a>
     </div>
   );

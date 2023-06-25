063dc48c964019ecfa73ee13d9e9b2cd37fccba6
Wantonfury <orzan.alexandru94@gmail.com>
2023-06-25 14:46:49 +0300

    Change Post model to not include a comments array

--- server/models/post.js
+++ server/models/post.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  message: { type: String, minLength: 5, maxLength: 255, required: true },
+  message: { type: String, minLength: 1, maxLength: 1000, required: true },
   likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   date: { type: Date, required: true },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  comments: [{
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    comment: { type: String, minLength: 5, maxLength: 100, required: true }
-  }],
   photo: { type: String }
 });
 
